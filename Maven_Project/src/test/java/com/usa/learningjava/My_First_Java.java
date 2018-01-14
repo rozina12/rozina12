af3f4b9c6b8c90835e2794d0d9a9b3f41a7bfd22
rozinazafar <rozinazafar@rozinaspc>
2018-01-13 20:50:01 -0500

    version 01

---
+++ Maven_Project/src/test/java/com/usa/learningjava/My_First_Java.java
@@ -0,0 +1,14 @@
+package com.usa.learningjava;
+
+public class My_First_Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Simple Java");
+	
+	
+	
+	}
+
+}
